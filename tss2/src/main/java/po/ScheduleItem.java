712f76cb6d46a73aed0b9f04221c56c300ef7ca7
hdx14 <hdx14@software.nju.edu.cn>
2016-07-15 09:30:47 +0800

    Development: 爬课表

---
+++ tss2/src/main/java/po/ScheduleItem.java
@@ -0,0 +1,41 @@
+package po;
+
+import java.util.List;
+
+public class ScheduleItem {
+	private String id;           //课程ID
+	    private String name;		//课程名
+	    private String message;
+	    private String teachers;		//课程老师
+
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public String getTeachers() {
+			return teachers;
+		}
+		public void setTeachers(String teachers) {
+			this.teachers = teachers;
+		}
+	    
+	    
+	    
+	
+	
+}
+
