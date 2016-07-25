9713fa314263658130f13948985a470c1e27bb9f
dyp14 <707969656@qq.com>
2016-07-25 10:01:29 +0800

    Development:修改mainacitivity

--- TssAndroid/app/src/main/java/com/example/tss/tssandroid/MainActivity.java
+++ TssAndroid/app/src/main/java/com/example/tss/tssandroid/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.tss.tssandroid;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -8,38 +13,139 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.tss.course.fragment.FragmentDisplyCourse;
+import com.example.tss.file.fragement.FragmentFileShow;
+import com.example.tss.login.activity.LoginActivity;
+import com.example.tss.message.activity.ConverListActivity;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialize.util.UIUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PROFILE_SETTING = 1;
 
-
+    //save our header or result
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
+    Fragment contentFra;
+    Fragment messageFra;
+    Fragment fileFra;
+    public Fragment courseFra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
 
 //        fm = getFragmentManager();
+        final IProfile profile = new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(R.drawable.profile);
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(true)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        profile,
+                        //don't ask but google uses 14dp for the add account icon in gmail but 20dp for the normal icons (like manage account)
+                        new ProfileSettingDrawerItem().withName("Add Account").withDescription("Add new GitHub Account").withIdentifier(PROFILE_SETTING),
+                        new ProfileSettingDrawerItem().withName("Manage Account")
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
 
+        //Create the drawer
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_message).withIcon(R.drawable.ic_menu_camera).withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_course).withIcon(R.drawable.ic_menu_gallery).withIdentifier(2),
+                        new PrimaryDrawerItem().withName(R.string.drawer_item_file).withIcon(R.drawable.ic_menu_camera).withIdentifier(3),
+                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
 
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(R.drawable.ic_menu_camera),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(R.drawable.ic_menu_camera),
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_logout).withIcon(R.drawable.ic_menu_camera).withIdentifier(6)
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        FragmentManager fm = getFragmentManager();
+                        FragmentTransaction transaction = fm.beginTransaction();
+                        if (drawerItem != null) {
+//                            startSupportActionMode(new ActionBarCallBack());
+//                            findViewById(R.id.action_mode_bar).setBackgroundColor(UIUtils.getThemeColorFromAttrOrRes(CompactHeaderDrawerActivity.this, R.attr.colorPrimary, R.color.material_drawer_primary));
+                            long identifier = drawerItem.getIdentifier();
+                            if(identifier == 1){
+                                startActivity(new Intent(MainActivity.this, ConverListActivity.class));
+                            }else if(identifier == 2){
+                                if(courseFra == null){
+                                    courseFra = new FragmentDisplyCourse();
+                                }
+                                transaction.replace(R.id.content,courseFra,"disply");
+                            }else if(identifier == 3){
+                                if(fileFra == null){
+//                                    stub s = new stub();
+//                                    fileFra = new FragmentFileShow(s.getMainFile());
+                                }
+                                transaction.replace(R.id.content,fileFra);
+                            }else if(identifier == 6){
+                                logout();
+                            }
+                        }
+
+                        if (drawerItem instanceof Nameable) {
+                            toolbar.setTitle(((Nameable) drawerItem).getName().getText(MainActivity.this));
+                        }
+
+                        transaction.addToBackStack(null);
+                        transaction.commit();
+
+                        return false;
+                    }
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        // set the selection to the item with the identifier 5
+        if (savedInstanceState == null) {
+            result.setSelection(5, false);
+        }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
     }
 
     @Override
@@ -71,5 +177,54 @@ public class MainActivity extends AppCompatActivity {
     public boolean onPrepareOptionsMenu(Menu menu) {
 
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    private void setDefaultFragment(){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        contentFra = new ContentFragment();
+        transaction.replace(R.id.content,contentFra);
+        transaction.commit();
+    }
+
+    void logout() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        String st = getResources().getString(R.string.Are_logged_out);
+        pd.setMessage(st);
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+        EMClient.getInstance().logout(true,new EMCallBack() {
+
+            @Override
+            public void onSuccess() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        pd.dismiss();
+                        // show login screen
+                        finish();
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+                    }
+                });
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        pd.dismiss();
+                        Toast.makeText(MainActivity.this, "unbind devicetokens failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
