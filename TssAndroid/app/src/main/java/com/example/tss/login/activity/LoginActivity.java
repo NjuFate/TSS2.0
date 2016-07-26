bde1858e1c2776871b96fcfcf23183edfefc8712
dyp14 <707969656@qq.com>
2016-07-26 16:00:56 +0800

    Development: 更改侧边栏图标和主页面配色

--- TssAndroid/app/src/main/java/com/example/tss/login/activity/LoginActivity.java
+++ TssAndroid/app/src/main/java/com/example/tss/login/activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
 import com.example.tss.tssandroid.MainActivity;
+
 import com.example.tss.tssandroid.R;
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
